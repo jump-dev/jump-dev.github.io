dc59f2c5118edc45c909515743cdfffa589f46e9
Oscar Dowson <odow@users.noreply.github.com>
2024-03-01 10:32:54 +1300

    Add preliminary schedule for JuMP-dev 2024 (#128)

---
+++ assets/jump-dev-workshops/build_schedule_table.jl
@@ -0,0 +1,57 @@
+import TOML
+
+get_key(day, time) = replace("$(day):$time", ":" => "_")
+
+function print_table(io, data)
+    talks = data["talks"]
+    day_names = data["days"]
+    indices = split.([k for k in keys(talks)], "_")
+    days = sort(unique(first.(indices)))
+    times = sort(unique([join(i[2:3], ":") for i in indices]))
+    println(io, """<table>\n<tr>\n  <th></th>""")
+    for day in days
+        d = get(day_names, parse(Int, day), day)
+        println(io, "  <th><b>$d</b></th>")
+    end
+    println(io, "</tr>")
+    for time in times
+        println(io, "<tr>")
+        println(io, """  <td class="talk-table">$time&nbsp;</td>""")
+        for day in days
+            content = ""
+            item = get(talks, get_key(day, time), nothing)
+            talk_type = ""
+            if item !== nothing
+                if haskey(item, "title")
+                    content *= """<div class="talk-title">$(item["title"])</div>"""
+                end
+                if haskey(item, "speaker")
+                    content *= """<div class="talk-speaker">$(item["speaker"])</div>"""
+                end
+                if haskey(item, "type")
+                    talk_type = " talk-" * item["type"]
+                end
+            end
+            class = "talk-table$talk_type"
+            println(io, """  <td class="$class">""", content, "</td>")
+        end
+        println(io, "</tr>")
+    end
+    println(io, "</table>")
+    return
+end
+
+function build_schedule(year::String)
+    root = dirname(dirname(@__DIR__))
+    data = TOML.parsefile(joinpath(@__DIR__, year, "schedule.toml"))
+    filename = joinpath(root, "_includes", "jump-dev-$year-schedule.html")
+    open(filename, "w") do io
+        print_table(io, data)
+    end
+    return
+end
+
+if !isempty(ARGS)
+    build_schedule(ARGS[1])
+end
+
